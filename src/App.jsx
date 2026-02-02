418478f238e3917dff6cd5a77eaa0c82cc6968ee
MhmdSalim <MhmdSalim@gmail.com>
2026-02-02 10:13:55 +0200

    Initial commit for my portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
-);
+const App = () => <Home />;
 
 export default App;
